65c5eb5ceed5ea023f114529ea361ca2c1aa24fa
tylergreason <tyler.greason@protonmail.com>
2020-03-03 11:10:35 -0500

    finished basic seed data for all tables

--- workout_backend/db/seeds.rb
+++ workout_backend/db/seeds.rb
@@ -1,37 +1,45 @@
 require 'faker'
 
+difficulty_range = ['easy','medium','hard']
+day = ["Monday", "Tuesday", "Wednesday", "Thurday", "Friday", "Saturday", "Sunday"]
+random_muscles = ["Bicep", "Tricep", "Abs", "Neck", "Quadriceps", "Hamstrings", "Calves", "Pelvis", "Pectorals", "Lats", "Deltoids", "Upper back", "Lower back"]
 
-new_user = User.create( 
-    first_name: "John", 
-    last_name: "Smith", 
-    username: "john_smith", 
-    password: "password"
-)
+# delete tables 
+User.all.delete_all 
+Workout.all.delete_all
+Muscle.all.delete_all
+Routine.all.delete_all
 
-new_workout = Workout.create(
-    name: "push ups", 
-    difficulty: "hard", 
-    description: "push yourself off the ground!" 
-)
 
-new_routine = Routine.create(
-    name: "Monday routine", 
-    description: "this is what I do on Mondays", 
-    date: "2020/01/01"
-)
+# create new workouts and give them muscles 
+20.times do 
+    new_workout = Workout.create(
+        name: Faker::Lorem.sentence(word_count: 3), 
+        difficulty: difficulty_range.sample, 
+        description: Faker::Lorem.sentence(word_count: 10)
+    )
+    2.times do
+        new_workout.muscles << Muscle.create(name:random_muscles.sample)
+    end
+end
+# create new users and give them routines
+5.times do 
+    new_user = User.create( 
+        first_name: Faker::Name.first_name, 
+        last_name: Faker::Name.last_name, 
+        username: Faker::Internet.username, 
+        password: "password"
+    )
+    # create routines, give them workouts, then give them users 
+    3.times do 
+        new_routine = Routine.create(name: day.sample + " workout", description: Faker::Lorem.sentence(word_count: 5), date: Faker::Date.in_date_period)
+        5.times do 
+            new_routine.workouts << Workout.all.sample
+        end
+        new_user.routines << new_routine 
+    end
+end
 
-new_muscle1 = Muscle.create(
-    name: "bicep"
-)
 
-new_muscle2 = Muscle.create(
-    name: "chest"
-)
-
-new_workout.muscles << new_muscle1
-new_workout.muscles << new_muscle2
-
-new_routine.workouts << new_workout
-
-new_user.routines << new_routine 
-
+# new_user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, username: Faker::Internet.username, password: "password")
+# new_routine = Routine.create(name: day.sample + " workout", description: Faker::Lorem.sentence(word_count: 5), date: Faker::Date.in_date_period)
